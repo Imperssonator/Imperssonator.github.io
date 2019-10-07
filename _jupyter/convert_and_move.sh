d0d26a7ee54dc5f9c24bc9bfff0d5426c6c9388e
Nils Persson <Imperssonator@users.noreply.github.com>
2019-10-07 07:18:06 -0400

    test post amazon

--- _jupyter/convert_and_move.sh
+++ _jupyter/convert_and_move.sh
@@ -1,5 +1,5 @@
 nb=$1
 
 jupyter nbconvert $nb --to markdown
-mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/
-mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/
+mv ${nb%.ipynb}.md ~/CC/imperssonator.github.io/_posts/
+mv ${nb%.ipynb}_files ~/CC/imperssonator.github.io/images/
